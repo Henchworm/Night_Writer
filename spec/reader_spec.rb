6ebabe4f5ead12fb31630fedda91519d9c9a05c1
Chris Hewitt <agop5134@gmail.com>
2021-09-27 14:19:13 -0600

    "cleaned up reader_spec/fix old test"

---
+++ spec/reader_spec.rb
@@ -0,0 +1,16 @@
+require './lib/reader'
+
+RSpec.describe Reader do
+
+  it "exists" do
+    reader = Reader.new
+    expect(reader).to be_a(Reader)
+  end
+
+  it "reads files and returns a string" do
+    reader = Reader.new
+    @input = ARGV[0]
+    characters = File.new(@input).read
+    expect(characters).to be_a(String)
+  end
+end

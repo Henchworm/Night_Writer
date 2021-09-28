fb001ac8100a4a36f96eeb158b4a0e978a8e47c1
Chris Hewitt <agop5134@gmail.com>
2021-09-27 21:22:08 -0600

    "Update night_reader_spec but needs work"

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -7,26 +7,21 @@ RSpec.describe NightReader do
     expect(nightreader).to be_a(NightReader)
   end
 
-  it "reads files and returns a string" do
+  it "reads files and returns a string + writes new file " do
     nightreader = NightReader.new
     @input = ARGV[0]
-    characters = File.new(@input).read
+    characters = File.new(@input).read_write
     expect(characters).to be_a(String)
+    # @output = ARGV[0]
+    # english = "bbbb"
+    # nightreader.write(english)
+    # expect(english).to be_a(String)
   end
-
-  xit "writes a string file" do
-    nightreader = NightReader.new
-    @output = ARGV[0]
-    english = "bbbb"
-    nightreader.write(english)
-    expect(english).to be_a(String)
-  end
+end
 
 
 
 
-
-  end
 
 
 

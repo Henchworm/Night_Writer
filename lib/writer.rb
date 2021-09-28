ba438120683f91bb2b6e2f76a8c412fd585c7127
Chris Hewitt <agop5134@gmail.com>
2021-09-28 04:14:59 -0600

    "Clean up"

--- lib/writer.rb
+++ lib/writer.rb
@@ -1,19 +1,13 @@
 class Writer
   def initialize
-    @writer = ARGV[1]
+    @input = ARGV[1]
     @char_counter = File.new(ARGV[0]).read.length
   end
 
   def write(braille)
-  File.open(@writer, 'w') do |file|
+  File.open(@input, 'w') do |file|
      file.puts braille
     end
-   puts "Created '#{@writer}' containing #{@char_counter} characters."
+   puts "Created '#{@input}' containing #{@char_counter} characters."
  end
-
 end
-
-# def line_break(braille_char)
-#   if braille_char.length > 100
-#     braille_char.slice(-1).sub("\n")
-#   end

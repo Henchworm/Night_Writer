5298a5ce2fdd3aa14cdb24d339fc3396fc337925
Chris Hewitt <agop5134@gmail.com>
2021-09-28 04:12:30 -0600

    "Clean up sapcing/remove junk"

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -18,29 +18,9 @@ class NightWriter
     characters.chars.each do |character|
       braille_chars << translate_to_braille(character)
     end
-    line_format_and_print(braille_chars)
+    line_format_and_print(braille_chars.compact)
   end
 
-  def line_format_and_print(braille_chars)
-    transpose_format = braille_chars.compact.transpose
-        joined = []
-        transpose_format.each do |element|
-          joined << element.join
-        end
-          joined.each do |element|
-            if element.length > 80
-              joined << element.slice!(80..-1)
-            end
-          end
-          # joined << joined[0].slice!(80..-1)
-          # joined << joined[1].slice!(80..-1)
-          # joined << joined[2].slice!(80..-1)
-          formatted_line = joined
-      @writer.write(formatted_line)
-    end
-
-
-  # @writer.write(braille_chars.compact.transpose.map{|row| row.join(" ")}.join("\n"))
   def translate_to_braille(character)
   braille_char = nil
   @braille_alphabet.alphabet.each do |char_key, array|
@@ -51,28 +31,21 @@ class NightWriter
     braille_char
   end
 
-#   def line_format_and_print(braille_chars)
-#     nest_format = braille_chars.compact.transpose
-#       nest_format.map do |n|
-#         if n.length > 3
-#           n.join.slice(5..-1)
-#         end
-#       end
-#       joined = []
-#       nest_format.each do |nest|
-#         joined << nest.join
-#       end
-#     @writer.write(joined)
-#     end
-#     require "pry"; binding.pry
-# end
+  def line_format_and_print(braille_chars)
+    transposed_format = braille_chars.transpose
+        joined = []
+        transposed_format.each do |element|
+          joined << element.join
+        end
+          joined.each do |element|
+            if element.length > 80
+              joined << element.slice!(80..-1)
+            end
+          end
+          formatted_line = joined
+      @writer.write(formatted_line)
+    end
 end
-
-
-
-
-# .compact.transpose.map{|row| row.join(" ")}.join("\n")
-
 NightWriter.new.import_transform_characters
 
 
